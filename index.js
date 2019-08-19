4b7e17629e865b130fa6ed3f6222fe99b371a6ed
Efim Poleschuk <limitlessFIMA@gmail.com>
2019-08-19 14:28:21 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const background = document.body;
-background.style.backgroundColor = 'white';
+const changeBackgroundColor = (color) => {
+  const background = document.body;
+  background.style.backgroundColor = color;
+};
 
-const userName = document.getElementsByClassName('page_name')[0];
-userName.innerHTML += ' <a class="page_verified"></a>';
+const addVerification = (name) => {
+  const userName = document.getElementsByClassName('page_name')[0];
+  const verifed = ' <a class="page_verified"></a>';
+
+  const page = window.location.pathname.slice(1, window.location.pathname.length);
+
+  if (page === name && userName.childNodes.length <= 1) {
+    userName.innerHTML += verifed;
+  }
+  if (page === name) {
+    const observer = new MutationObserver((() => {
+      addVerification('limitlessfima');
+    }));
+    observer.observe(document.body, {
+      attributes: true,
+    });
+  }
+};
+
+
+addVerification('limitlessfima');
+changeBackgroundColor('white');
